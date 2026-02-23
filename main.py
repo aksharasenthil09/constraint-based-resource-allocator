a246556fd4db2fe19a24f04519caa47719ccd230
Akshara Senthil <aksharasenthil.09@gmail.com>
2026-02-23 17:03:52 +0530

    Implement Task model with remaining effort and success confirmation

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Project initialised successfully")
+from models import Task
+
+def main():
+    math=Task(
+        name="Math",
+        due_day=5,
+        total_effort=20,
+        current_preparedness=40,
+        threshold=75
+    )
+
+    print("Task:", math)
+    print("Remaining effort: ", math.remaining_effort())
+    print("Is successful:", math.is_successful())
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,34 @@
+class Task:
+    def __init__(
+            self,
+            name: str,
+            due_day: int,
+            total_effort: float,
+            current_preparedness: float,
+            threshold: float
+    ):
+        """
+        Represents a single task, which in most cases will be an exam
+        """
+        self.name=name
+        self.due_day=due_day
+        self.total_effort=total_effort
+        self.current_preparedness=current_preparedness
+        self.threshold=threshold
+
+    def remaining_effort(self) -> float:
+        """
+        Calculates how much effort is still required to reach 100% preparedness
+        """
+        remaining_fraction=max(0,1-self.current_preparedness/100)
+        return remaining_fraction * self.total_effort
+    
+    def is_successful(self) -> bool:
+        """
+        Returns true if task meets threshold
+        """
+        return self.current_preparedness >= self.threshold
+    
+    def __str__(self):
+        return f"{self.name} | Preparedness: {self.current_preparedness}"
+    
